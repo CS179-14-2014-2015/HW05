dafe507441b3fe962e262ed14c8d87a1d663d906
hadrianpaulo <hadrianpaulo@gmail.com>
2015-01-13 11:43:50 +0800

    fixed collision detection

--- Pepero.cpp
+++ Pepero.cpp
@@ -1,13 +1,12 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <stdio.h>
-#include <string>
 #include <vector>
 #include <cstdlib>
+#include <cmath>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 SDL_Window* gWindow = nullptr;
 SDL_Renderer* renderer = nullptr;
@@ -15,24 +14,51 @@ SDL_Renderer* renderer = nullptr;
 // Class Declaration
 class Ball
 {
-public:
-  static const int bRadius = 2;
+ public:
+
+  // Ball dimensions
+  int bRadius = 5;
+  int bMass = 10; // in grams
   int bPosX, bPosY;
   int velX, velY;
 
+  // Unique ID
+  const int uid;
+  int hitByID = 0;
+
   Ball();
+  void checkBallBoundaries(Ball& );
   void collisionDetection(Ball& );
-  double distanceSquared( int , int , int , int  );
+  void determineCollisionPoint(Ball& );
+  double distance( int , int , int , int  );
   void render();
   void move();
   ~Ball();
 
+  private:
+     // uid hack
+    static int newUID;
+    // Ball Boundaries Flag
+    bool overlappedBoundary;
+    // Collision Points
+    int collisionX;
+    int collisionY;
+    // Collision Velocity Holder
+    int newVelX = 0;
+    int newVelY = 0;
+    // colorsssss
+    int red = rand() % 255;
+    int green = rand() % 255;
+    int blue = rand() % 255;
 };
+
+// Ball UID hack
+int Ball::newUID = 0;
 
 class BallGroup
 {
   public:
-   static const int ballAmount = 10;
+   static const int ballAmount = 100;
    std::vector<Ball> BallGroupContainer;
 
    BallGroup();
@@ -44,67 +70,129 @@ class BallGroup
 
 // Ball Class Implementation and methods
 
-Ball::Ball()
+Ball::Ball(): uid(newUID++)
 {
 // Starting position
-  bPosX = rand() % (SCREEN_WIDTH - bRadius) + bRadius;
-  bPosY = rand() % (SCREEN_HEIGHT - bRadius) + bRadius;
+  bPosX = rand() % int(SCREEN_WIDTH/1.5) + bRadius;
+  bPosY = rand() % int(SCREEN_HEIGHT/1.5) + bRadius;
 
 // Velocity
   velX = rand() % 9 + 1;
-  velY = rand() % 9 + 1;;
+  velY = rand() % 9 + 1;
 }
 
+void Ball::checkBallBoundaries(Ball& otherBall)
+{
+ if (this->bPosX + this->bRadius + otherBall.bRadius > otherBall.bPosX
+  && this->bPosX < otherBall.bPosX + this->bRadius + otherBall.bRadius
+  && this->bPosY + this->bRadius + otherBall.bRadius > otherBall.bPosY
+  && this->bPosY < otherBall.bPosY + this->bRadius + otherBall.bRadius)
+ {
+     this->overlappedBoundary = true;
+     printf("%d", this->overlappedBoundary);
+     printf("\n");
+ }
+ else
+ {
+     this->overlappedBoundary = false;
+     printf("%d", this->overlappedBoundary);
+     printf("\n");
+ }
+}
 
 void Ball::collisionDetection(Ball& otherBall)
 {
-  int totalRadiusSquared = this->bRadius + otherBall.bRadius;
-  totalRadiusSquared *= totalRadiusSquared;
+  this->checkBallBoundaries(otherBall);
 
-  if(distanceSquared(this->bPosX, this->bPosY, otherBall.bPosX, otherBall.bPosY) < totalRadiusSquared)
+  if (this->overlappedBoundary && this->uid != otherBall.uid)
   {
-    if(this->bPosX == otherBall.bPosX)
-    {
-        this->velY *= -1;
-    }
-    else if (this->bPosY == otherBall.bPosY)
-    {
-        this->velX *= -1;
-    }
-    else
-    {
-        this->velX *= -1;
-        this->velY *= -1;
+   // Reset flag
+   this->overlappedBoundary = false;
 
-    }
+   int totalRadius = this->bRadius + otherBall.bRadius;
+
+   if(distance(this->bPosX, this->bPosY, otherBall.bPosX, otherBall.bPosY) < totalRadius)
+   {
+     this->determineCollisionPoint(otherBall);
+     // Determine new velocities
+     this->newVelX = (this->velX * (this->bMass - otherBall.bMass) + (2 * otherBall.bMass * otherBall.velX)) / (this->bMass + otherBall.bMass);
+     this->newVelY = (this->velY * (this->bMass - otherBall.bMass) + (2 * otherBall.bMass * otherBall.velY)) / (this->bMass + otherBall.bMass);
+     otherBall.newVelX = (otherBall.velX * (otherBall.bMass - this->bMass) + (2 * this->bMass * this->velX)) / (this->bMass + otherBall.bMass);
+     otherBall.newVelY = (otherBall.velY * (otherBall.bMass - this->bMass) + (2 * this->bMass * this->velY)) / (this->bMass + otherBall.bMass);
+
+     // Adjust ball positions
+     this->bPosX += this->newVelX;
+     this->bPosY += this->newVelY;
+     otherBall.bPosX += otherBall.newVelX;
+     otherBall.bPosY += otherBall.newVelY;
+
+     // Adjust speed
+     this->velX = this->newVelX;
+     this->velY = this->newVelY;
+     otherBall.velX = otherBall.newVelX;
+     otherBall.velY = otherBall.newVelY;
+
+     // Reset velocity holders
+     this->newVelX = 0;
+     this->newVelY = 0;
+     otherBall.newVelX = 0;
+     otherBall.newVelY = 0;
+   }
   }
+}
+
+void Ball::determineCollisionPoint(Ball& otherBall)
+{
+ this->collisionX =
+ ((this->bPosX * otherBall.bRadius) + (otherBall.bPosX * this->bRadius))
+ / (this->bRadius + otherBall.bRadius);
+
+ this->collisionY =
+ ((this->bPosY * otherBall.bRadius) + (otherBall.bPosY * this->bRadius))
+ / (this->bRadius + otherBall.bRadius);
+
 
 }
 
 void Ball::move(){
 
-  if ((bPosX <= 0+bRadius) || (bPosX >= SCREEN_WIDTH-bRadius))
+  if (bPosX <= 0+bRadius)
   {
+    this->bPosX = bRadius;
     velX *= -1;
   }
-  if ((bPosY <= 0+bRadius) || (bPosY >= SCREEN_HEIGHT-bRadius))
+  if  (bPosX >= SCREEN_WIDTH-bRadius)
   {
+    this->bPosX = SCREEN_WIDTH-bRadius;
+    velX *= -1;
+  }
+
+  if (bPosY <= 0+bRadius)
+  {
+    this->bPosY = bRadius;
+    velY *= -1;
+  }
+  if (bPosY >= SCREEN_HEIGHT-bRadius)
+  {
+    this->bPosY = SCREEN_HEIGHT-bRadius;
     velY *= -1;
   }
   bPosX += velX;
   bPosY += velY;
 }
 
-double Ball::distanceSquared( int x1, int y1, int x2, int y2 )
+double Ball::distance( int x1, int y1, int x2, int y2 )
 {
-  int deltaX = x2 - x1;
-  int deltaY = y2 - y1;
-  return deltaX*deltaX + deltaY*deltaY;
+  int distance = sqrt(
+            ((x1 - x2) * (x1 - x2))
+          + ((y1 - y2) * (y1 - y2))
+           );
+  return distance;
 }
 
 void Ball::render()
 {
-  filledCircleRGBA(renderer, bPosX, bPosY, bRadius, 255, 255, 255, 255);
+  filledCircleRGBA(renderer, bPosX, bPosY, bRadius, red, green, blue, 255);
 }
 
 Ball::~Ball(){}
@@ -130,9 +218,8 @@ void BallGroup::render(){
 
 void BallGroup::collisionDetection(){
     for (auto &pCircle : BallGroupContainer){
-        for (auto &tCircle : BallGroupContainer){
+      for(auto &tCircle : BallGroupContainer)
             pCircle.collisionDetection(tCircle);
-        }
     }
 }
 
@@ -177,13 +264,11 @@ bool init()
       }
     }
   }
-
   return success;
 }
 
 void close()
 {
-
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(gWindow);
   gWindow = NULL;
@@ -191,12 +276,6 @@ void close()
 
   SDL_Quit();
 }
-
-
-/*
-* MAIN
-*/
-
 
 int main(int argc, char* args[])
 {
@@ -213,8 +292,8 @@ int main(int argc, char* args[])
     while(!quit)
     {
       uint capTimer = SDL_GetTicks();
-      //lol.collisionDetection();
       lol.move();
+      lol.collisionDetection();
       while(SDL_PollEvent(&e) != 0)
       {
         if(e.type == SDL_QUIT)
@@ -222,19 +301,15 @@ int main(int argc, char* args[])
           quit = true;
         }
       }
-// Clear Screen
+      // Clear Screen
       SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
       SDL_RenderClear(renderer);
-
-// Render Objects
+      // Render Objects
       lol.render();
-
-// Render other objects
+      // Render other objects
       SDL_RenderPresent(renderer);
-
     }
   }
-
   close();
   return 0;
 }
