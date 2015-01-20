91ad61d8870b04b595da3fddb828d4266624628f
Guillaume Robbe <guillaume@guillaume-latptop.(none)>
2015-01-20 13:16:25 +0800

    Project Cleaned for submission.

--- Debug/src/BouncingBalls.d
+++
@@ -1,6 +0,0 @@
-src/BouncingBalls.d: ../src/BouncingBalls.cpp ../src/Ball.h \
- ../src/Vector2D.h
-
-../src/Ball.h:
-
-../src/Vector2D.h:
--- Release/src/Ball.d
+++
@@ -1,8 +0,0 @@
-src/Ball.d: ../src/Ball.cpp ../src/Ball.h ../src/Vector2D.h \
- ../src/Tools.h
-
-../src/Ball.h:
-
-../src/Vector2D.h:
-
-../src/Tools.h:
